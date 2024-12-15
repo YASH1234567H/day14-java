64b84681b47537613d4c5ebfb786955cdeb8a3fc
Yashwant <anyname6281@gmail.com>
2024-12-15 10:34:54 +0530

    Add files via upload

---
+++ thread2.java
@@ -0,0 +1,18 @@
+//implemnting runnable
+class a implements Runnable
+{
+    public void run()
+    {
+        System.out.println("Hello I'm Thread");
+    }
+}
+class main
+{
+    public static void main(String args[])
+    {
+        a r=new a();
+       Thread s=new Thread(r);
+       s.start();
+       s.run();
+    }
+}
