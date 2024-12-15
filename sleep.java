6d5721cb601eabcac1a56431f10f27e954a7ead3
Yashwant <anyname6281@gmail.com>
2024-12-15 11:07:23 +0530

    Add files via upload

---
+++ sleep.java
@@ -0,0 +1,17 @@
+//creating thread using lamda 
+class main
+{
+    public static void main(String args[])
+    {
+        System.out.println("Thread Started...");
+        try
+        {
+            Thread.sleep(10000);
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Interupted");
+        }
+        System.out.println("Thread resumed");
+    }
+}
