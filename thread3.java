c1db3ada4240a4cc5ded6d52bf9dfca6562a1503
Yashwant <anyname6281@gmail.com>
2024-12-15 10:38:09 +0530

    Add files via upload

---
+++ thread3.java
@@ -0,0 +1,10 @@
+//creating thread using lamda 
+class main
+{
+    public static void main(String args[])
+    {
+        Runnable r= ()->System.out.println("I am new from java8 version");
+        Thread t=new Thread(r);
+        t.start();
+    }
+}
